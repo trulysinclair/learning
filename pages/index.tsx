d947e7770a7a045f6fe46baea9ebfd075e556a04
NuclearPickleTV <90234811+NuclearPickleTV@users.noreply.github.com>
2022-12-27 15:48:34 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,26 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    // MAIN SCREEN CONTAINER //
     <div className='flex flex-col flex-wrap bg-back-black justify-center w-screen h-screen items-center'>
-      <div className='flex flex-col drop-shadow-md rounded-lg bg-bottom-black w-1/5 h-1/2 text-white'>
+      // Centered container in the middle of the screen //
+      <div className='flex flex-col drop-shadow-md rounded-lg bg-bottom-black w-1/5 h-1/3 text-white items-center'>
+        // Container for the title bar at the top of the container //
         <div className='flex flex-col w-full h-16 justify-center drop-shadow-md items-center rounded-t-lg bg-upper-black' >
           <h1 className='text-3xl font-bold text-white'>Login</h1>
         </div>
-        <div className='flex justify pl-5 pt-5'>
-          <label htmlFor='email' className='font'>Email</label>
-          <input></input>
-        </div>    
+        // Div for email label and input //
+        <div className='flex flex-col w-full justify pl-5 pt-5 pr-5'>
+          <label htmlFor='email' className='font pb-1'>Email</label>
+          <input className='text-black rounded-md h-8 drop-shadow-md border-gray-300' placeholder='user@email.com'></input>
+        </div>
+        // Div for password label and input //
+        <div className='flex flex-col w-full justify pl-5 pt-3 pr-5'>
+          <label htmlFor='password' className='font pb-1'>Password</label>
+          <input type='password' className='text-black rounded-md h-8 drop-shadow-md border-gray-300' placeholder='password'></input>
+        </div>
+        // Login button //
+        <button className=' rounded-md bg-green-600 mt-8 mb-8 w-24 h-8'>Log In</button>        
       </div>
     </div>
   )
