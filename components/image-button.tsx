4dcc57347bac39d75608b5816934335721aa234d
NuclearPickleTV <90234811+NuclearPickleTV@users.noreply.github.com>
2022-12-28 21:15:24 -0600

    Fix onclick

--- components/image-button.tsx
+++ components/image-button.tsx
@@ -8,12 +8,12 @@ import {
 	ReactPortal,
 } from "react";
 
-export default function ImageButton(props: { data: string, label: string }) {
+export default function ImageButton(props: { data: string, label: string, onClick: () => void }) {
     return (
-        <button className="justify-center items-center bg-gray-500 h-14 w-full flex flex-row text-white relative">
+		<button className="justify-center items-center bg-gray-500 h-14 w-full flex flex-row text-white relative" onClick={props.onClick}>
 			<span className="absolute top-0 left-0 bottom-0 flex justify-center items-center">
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="pl-5 h-6">
-  					<path stroke-linecap="round" stroke-linejoin="round" d={props.data}/>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="pl-5 h-6">
+					<path strokeLinecap="round" strokeLinejoin="round" d={props.data}/>
 				</svg>
 			</span>
 			<h2 className="flex text-xl justify-center text-white">{props.label}</h2>
